8978e5c58311d7964720485e1de5122a8675c5d2
Everton de Almeida Campos Oliveira <everton17@outlook.com>
2020-08-18 02:38:49 -0300

    Atualização do conteudo da aula 2.
    Atualização do conteudo da aula 2.

---
+++ src/br/com/cursoemvideo/algoritimo/aulas/AulaDoisPrimeiroAlgoritimo.java
@@ -0,0 +1,13 @@
+package br.com.cursoemvideo.algoritimo.aulas;
+
+public class AulaDoisPrimeiroAlgoritimo {
+	
+	public static void main(String[] args) {
+		
+		String msg;
+		msg = "Olá, Mundo!";
+		
+		System.out.println("Mensagem: " + msg);
+	}
+
+}
--- src/br/com/cursoemvideo/algoritimo/teste/Teste.java
+++ src/br/com/cursoemvideo/algoritimo/teste/Teste.java
@@ -5,6 +5,7 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		System.out.println("Olá Mundo!");
+		System.out.println("Me livrei da maldição!");
 	}
 
 }
